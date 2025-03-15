cccef47a50b45889bbb73f888f262c31021c73ff
nvietanh2406@github.com <nvietanh1988@gmail.com>
2025-03-15 14:05:17 +0000

    change file deploy

--- nfs/deploy.sh
+++ nfs/deploy.sh
@@ -1,10 +1,15 @@
+helm list -A | grep nfs
+helm uninstall nfs-provisioner -n default
+kubectl delete clusterrole nfs-provisioner-nfs-subdir-external-provisioner-runner
+kubectl delete clusterrolebinding nfs-provisioner-nfs-subdir-external-provisioner-runner
+kubectl delete namespace nfs
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm repo update
 helm install nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --namespace nfs \
+  --create-namespace \
   --set nfs.server=10.32.1.100 \
   --set nfs.path=/mnt/nfs-share \
-  --set storageClass.name=nfs-storage \
-  --set storageClass.defaultClass=false
 
 kubectl apply -f nfs-retain.yaml
-kubectl apply -f nfs-delete.yaml
+#kubectl apply -f nfs-delete.yaml
